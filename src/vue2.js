0616433c952efa8ec858eafac0af7a2071d5e3eb
Andy Wood <andy@bitshaper.co.uk>
2022-04-21 11:24:43 +0100

    Try to match module JS style.

--- src/vue2.js
+++ src/vue2.js
@@ -1,61 +1,56 @@
-import { renderPattern, simulateLoading } from './storybook-django'
+import { renderPattern, simulateLoading } from './storybook-django';
 
 const getTemplateName = (template, filename) =>
   template ||
   filename?.replace(/.+\/templates\//, '').replace(/\.stories\..+$/, '.html') ||
-  'template-not-found'
+  'template-not-found';
 
 export const DjangoPattern = {
   name: 'DjangoPattern',
   props: {
     element: {
       type: String,
-      default: 'div'
+      default: 'div',
     },
     template: {
-      type: String
+      type: String,
     },
     filename: {
-      type: String
+      type: String,
     },
     context: {
       type: Object,
-      default: () => ({})
+      default: () => ({}),
     },
     tags: {
       type: Object,
-      default: () => ({})
+      default: () => ({}),
     },
     endpoint: {
       type: String,
-      default: '/pattern-library/api/v1/render-pattern'
+      default: '/pattern-library/api/v1/render-pattern',
     }
   },
   watch: {
     context: {
       deep: true,
       immediate: true,
-      handler (val) {
-        this.getRenderedPattern()
-      }
-    }
-  },
-  methods: {
-    async getRenderedPattern () {
-      const templateName = getTemplateName(this.template, this.filename)
+      async handler (val) {
+        const templateName = getTemplateName(this.template, this.filename);
 
-      try {
-        const res = await renderPattern(this.endpoint, templateName, this.context, this.tags)
-        const html = await res.text()
-        simulateLoading(this.$refs.elt, html)
-      } catch (err) {
-        simulateLoading(this.$refs.elt, err)
-      }
+        try {
+          const res = await renderPattern(this.endpoint, templateName, this.context, this.tags);
+          const html = await res.text();
+          simulateLoading(this.$refs.elt, html);
+        } catch (err) {
+          simulateLoading(this.$refs.elt, err);
+        }
+      },
     }
   },
   render (createElement) {
     return createElement(this.element, {
-      ref: 'elt'
-    })
+      ref: 'elt',
+    });
   }
-}
+};
